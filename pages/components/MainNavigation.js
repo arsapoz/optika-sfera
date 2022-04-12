d3457ca54e2d9e9bffcd0511120bb850409aea86
julijana.mandic <julijana.mandic@ikor.de>
2022-04-12 19:24:37 +0200

    changed logo

--- pages/components/MainNavigation.js
+++ pages/components/MainNavigation.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import Image from 'next/image'
+import { Icon } from '@iconify/react';
+import tailwindcssIcon from '@iconify/icons-logos/tailwindcss';
 import { Disclosure } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
@@ -36,16 +37,7 @@ const MainNavigation = () => {
                             </div>
                             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex-shrink-0 flex items-center">
-                                    <Image
-                                        className="block lg:hidden h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                                        alt="Workflow"
-                                    />
-                                    <Image
-                                        className="hidden lg:block h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                                        alt="Workflow"
-                                    />
+                                <Icon icon={tailwindcssIcon} />
                                 </div>
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
@@ -65,7 +57,7 @@ const MainNavigation = () => {
                                     </div>
                                 </div>
                             </div>
-                           
+
                         </div>
                     </div>
 
